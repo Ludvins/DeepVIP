Fix variance term and generative model

src/dvip.py
@@ -222,14 +222,15 @@ class DVIP_Base(tf.keras.Model):
         # Return arrays
         return Fs, Fmeans, Fvars
 
-    def predict_f(self, predict_at, num_samples = 1, full_cov=False):
+    def predict_f(self, predict_at, num_samples=1, full_cov=False):
         _, Fmeans, Fvars = self.propagate(
             predict_at, full_cov=full_cov, num_samples=num_samples
         )
         return Fmeans[-1], Fvars[-1]
 
     def predict_all_layers(self, predict_at, num_samples, full_cov=False):
-        return self.propagate(predict_at, full_cov=full_cov, num_samples=num_samples)
+        return self.propagate(predict_at, full_cov=full_cov,
+                              num_samples=num_samples)
 
     def predict_y(self, predict_at, num_samples=1):
         Fmean, Fvar = self.predict_f(
@@ -239,7 +240,6 @@ class DVIP_Base(tf.keras.Model):
 
     def predict_log_density(self, data, num_samples):
         Fmean, Fvar = self.predict_f(data[0], num_samples=num_samples, full_cov=False)
-        # TODO cambiar nombre a predict log density
         l = self.likelihood.predict_density(Fmean, Fvar, data[1])
         log_num_samples = tf.math.log(tf.cast(self.num_samples, self.dtype))
         return tf.reduce_logsumexp(l - log_num_samples, axis=0)
@@ -284,3 +284,7 @@ class DVIP_Base(tf.keras.Model):
         KL = tf.reduce_sum([layer.KL() for layer in self.vip_layers])
 
         return -scale * likelihood + KL
+
+    def print_variables(self):
+        for variable in self.trainable_variables:
+            print(variable)

src/generative_models.py
@@ -1,37 +1,77 @@
 import tensorflow as tf
 
 
-class Linear(tf.keras.layers.Layer):
-    def __init__(self, noise_generator, num_samples, num_outputs=1, input_dim=32):
+class Generative_function(tf.keras.layers.Layer):
+    def __init__(self, noise_generator, num_samples, num_outputs, input_dim):
+
         """
-        TODO num_samples should be told by dvip, not in the main script
+
+        Parameters:
+        -----------
+        noise_generator :
+
+        num_samples :
+
+        num_outputs :
+
+        input_dim :
+
         """
-        super(Linear, self).__init__(dtype="float64")
+        super(Generative_function, self).__init__(dtype="float64")
+        self.num_samples = num_samples
+        self.num_outputs = num_outputs
+        self.input_dim = input_dim
         self.noise_generator = noise_generator
 
-        w_init = tf.random_normal_initializer()
-        self.w = tf.Variable(
-            initial_value=w_init(shape=(input_dim, num_outputs), dtype="float64"),
+    def call(self, inputs):
+        """
+
+        Parameters:
+        -----------
+        inputs :
+        """
+        raise NotImplementedError
+
+
+class BayesianLinearNN(Generative_function):
+    def __init__(self, noise_generator, num_samples, num_outputs=1, input_dim=32):
+
+        initializer = tf.random_normal_initializer(mean=0, stddev=1.0)
+        self.w_mean = tf.Variable(
+            initial_value=0.01
+            * initializer(shape=(input_dim, num_outputs), dtype="float64"),
             trainable=True,
-            name="Generative function weights",
+            name="w_mean",
         )
 
-        b_init = tf.zeros_initializer()
-        self.b = tf.Variable(
-            initial_value=b_init(shape=(num_outputs), dtype="float64"),
+        self.w_log_std = tf.Variable(
+            initial_value=-5
+            + initializer(shape=(input_dim, num_outputs), dtype="float64"),
             trainable=True,
-            name="Generative function bias",
+            name="w_log_std",
         )
 
-        self.num_samples = num_samples
+        self.b_mean = tf.Variable(
+            initial_value=0.01 * initializer(shape=[num_outputs],
+                                             dtype="float64"),
+            trainable=True,
+            name="b_mean",
+        )
 
-        self.z_w = self.noise_generator((num_samples, 1, num_outputs))
-        self.z_b = self.noise_generator((num_samples, 1, num_outputs))
+        self.b_log_std = tf.Variable(
+            initial_value=-5 + initializer(shape=[num_outputs],
+                                           dtype="float64"),
+            trainable=True,
+            name="b_std",
+        )
+        super().__init__(noise_generator, num_samples, num_outputs, input_dim)
 
-    def call(self, inputs, num_samples):
-        wx = tf.matmul(inputs, self.w)
-        wx = tf.expand_dims(wx, 0)
-        wx = tf.tile(wx, [self.num_samples, 1, 1])
-        wx = self.z_w * wx
-        b = self.z_b * self.b
-        return wx + b
+    def call(self, inputs):
+
+        z_w = self.noise_generator((self.num_samples, 1, self.num_outputs))
+        z_b = self.noise_generator((self.num_samples, 1, self.num_outputs))
+
+        w = z_w * tf.math.exp(self.w_log_std) + self.w_mean
+        b = z_b * tf.math.exp(self.b_log_std) + self.b_mean
+
+        return inputs @ w + b

src/layers.py
@@ -2,7 +2,6 @@ import tensorflow as tf
 
 from .utils import reparameterize
 
-from tensorflow.python.keras.backend import batch_normalization
 import tensorflow_probability as tfp
 import numpy as np
 
@@ -20,6 +19,8 @@ class Layer(tf.keras.layers.Layer):
         dtype : data-type
                 The dtype of the layer's computations and weights.
                 Refer to tf.keras.layers.Layer for more information.
+
+
         **kwargs : dict, optional
                    Extra arguments to `Layer`.
                    Refer to tf.keras.layers.Layer for more information.
@@ -83,8 +84,10 @@ class Layer(tf.keras.layers.Layer):
         """
 
         if full_cov:
+
             def f(a):
                 return self.conditional_ND(a, full_cov=True)
+
             mean, var = tf.map_fn(f, X, dtype=(tf.float64, tf.float64))
             return tf.stack(mean), tf.stack(var)
         else:
@@ -94,9 +97,8 @@ class Layer(tf.keras.layers.Layer):
 
             X_flat = tf.reshape(X, [S * N, D])
             mean, var = self.conditional_ND(X_flat, full_cov=False)
-            # TODO Check this return shape
-            return [tf.reshape(m, [S, N, self.num_outputs])
-                    for m in [mean, var]]
+
+            return [tf.reshape(m, [S, N, self.num_outputs]) for m in [mean, var]]
 
     @tf.function
     def sample_from_conditional(self, X, z=None, full_cov=False):
@@ -134,8 +136,8 @@ class Layer(tf.keras.layers.Layer):
 
         # If no sample is given, generate it from a standardized Gaussian
         if z is None:
-            z = tf.random.normal(shape=tf.shape(mean),
-                                 seed=self.seed, dtype=self.dtype)
+            z = tf.random.normal(shape=tf.shape(mean), seed=self.seed,
+                                 dtype=self.dtype)
         # Apply re-parameterization trick to z
         samples = reparameterize(mean, var, z, full_cov=full_cov)
 
@@ -164,19 +166,18 @@ class Layer(tf.keras.layers.Layer):
 class VIPLayer(Layer):
     def __init__(
         self,
-        generative_f,
-        layer_noise,
+        noise_generator,
+        generative_function,
         num_regression_coeffs,
         num_outputs,
         input_dim,
+        log_layer_noise = -5,
         mean_function=None,
         dtype=tf.float64,
         **kwargs
     ):
         """
-        A variational implicit process layer in whitened representation.
-        This layer holds the kernel, variational parameters,
-        linear regression coefficients and mean function.
+        A variational implicit process layer.
 
         The underlying model performs a Bayesian linear regression
         approximation
@@ -204,6 +205,8 @@ class VIPLayer(Layer):
 
         Parameters
         ----------
+
+
         generative_f : callable
                        Generates function samples using the input locations X.
 
@@ -243,15 +246,22 @@ class VIPLayer(Layer):
         self.q_mu = tf.Variable(q_mu, name="q_mu")
 
         # Store class members
-        if mean_function == None:
+        if mean_function is None:
             self.mean_function = lambda x: 0
         else:
             self.mean_function = mean_function
 
         self.num_outputs = tf.constant(num_outputs, dtype=tf.int32)
-        self.generative_f = generative_f
+        self.generative_function = generative_function(
+            noise_generator, num_regression_coeffs, num_outputs, input_dim
+        )
 
-        self.layer_noise = layer_noise
+        self.log_layer_noise = tf.Variable(
+            initial_value=log_layer_noise,
+            dtype="float64",
+            trainable=True,
+            name="layer_log_noise",
+        )
 
         # Define Regression coefficients deviation using tiled triangular
         # identity matrix
@@ -317,22 +327,24 @@ class VIPLayer(Layer):
         """
 
         # Shape (num_coeffs, N, D)
-        f = self.generative_f(X, self.num_coeffs)
+        f = self.generative_function(X)
         # Compute mean value, shape (N, D)
         m = tf.reduce_mean(f, axis=0)
 
         # Compute regresion function, shape (num_coeffs, N, D)
         # NOTE Poner estimador insesgado (S-1) (?)
-        sqrt = 1 / tf.math.sqrt(tf.cast(self.num_coeffs, dtype="float64"))
-        phi = sqrt * (f - m)
+        inv_sqrt = 1 / tf.math.sqrt(tf.cast(self.num_coeffs, dtype="float64"))
+        phi = inv_sqrt * (f - m)
 
-        # Compute mean value using q_mu^T phi
-        # q_mu has shape (num_coeffs, D), now (num_coeffs, 1, D)
-        q_mu = tf.expand_dims(self.q_mu, 1)
-        # shape (num_coeffs, N, D)
-        q_mu = tf.tile(q_mu, [1, phi.shape[1], 1])
-        # Shape (N, D)
-        mean = m + tf.reduce_sum(q_mu * phi, axis=0)
+        # Compute mean value as m + q_mu^T phi per point and output dim
+        # q_mu has shape (num_coeffs, N, num_outputs)
+        # phi has shape (num_coeffs, num_outputs)
+        mean = m + tf.einsum("snd,sd->nd", phi, self.q_mu)
+
+        # Shape (num_outputs, num_coeffs, num_coeffs)
+        q_sqrt = tfp.math.fill_triangular(self.q_sqrt_tri)
+        # Shape (num_outputs, num_coeffs, num_coeffs)
+        Delta = tf.matmul(q_sqrt, q_sqrt, transpose_b=True)
 
         if full_cov:
             # Shape (N, N, D)
@@ -340,9 +352,9 @@ class VIPLayer(Layer):
             var = self.layer_noise + phi
         else:
             # Shape (N, D)
-            # TODO Square this?
-            K = tf.math.reduce_std(phi, 0)
-            var = K + self.layer_noise
+            K = tf.einsum("snd,dsk->snd", phi, Delta)
+            K = tf.einsum("snd,snd->nd", K, phi)
+            var = K + tf.math.exp(self.log_layer_noise)
 
         return mean + self.mean_function(X), var
 
@@ -352,12 +364,12 @@ class VIPLayer(Layer):
         Computes the KL divergence from the variational distribution of
         the linear regression coefficients to the prior.
 
-        That is from a Gaussian N(a_mu, a_sqrt) to N(0, I).
+        That is from a Gaussian N(q_mu, q_sqrt) to N(0, I).
         Uses formula for computing KL divergence between two
         multivariate normals, which in this case is:
         \[
-            KL = 0.5* ( tr(a_sqrt^T a_sqrt) +
-                 a_mu^T a_mu - M - \log |a_sqrt^T a_sqrt| )
+            KL = 0.5* ( tr(q_sqrt^T q_sqrt) +
+                 q_mu^T q_mu - M - \log |q_sqrt^T q_sqrt| )
         \]
         """
 
@@ -373,9 +385,9 @@ class VIPLayer(Layer):
         # Uses that sqrt(det(X)) = det(X^(1/2)) and
         # that the determinant of a upper triangular matrix (which a_sqrt is),
         # is the product of the diagonal entries (i.e. sum of their logarithm).
-        KL -= 0.5 * tf.reduce_sum(tf.math.log(tf.linalg.diag_part(q_sqrt) ** 2))
+        KL -= 0.5 * tf.reduce_sum(2*tf.math.log(tf.linalg.diag_part(q_sqrt)))
 
-        # Trace term
+        # Trace term, check this
         KL += 0.5 * tf.reduce_sum(tf.linalg.diag_part(tf.square(q_sqrt)))
 
         # Mean term

src/likelihood.py
@@ -5,21 +5,57 @@ import tensorflow_probability as tfp
 import numpy as np
 
 
-class Gaussian(tf.keras.layers.Layer):
-    def __init__(self, variance=1.0, dtype=tf.float64, **kwargs):
-        """"""
-        super().__init__(dtype=dtype)
+class Likelihood(tf.keras.layers.Layer):
+    def __init__(self, dtype=tf.float64, **kwargs):
+        """
 
-        transform = tfp.bijectors.Softplus()
-        self.variance = tfp.util.TransformedVariable(
-            variance, bijector=transform, dtype=self.dtype, name="lik_log_variance"
+        """
+        super().__init__(dtype=dtype)
+        self.build(0)
+
+    @property
+    def metrics(self):
+        raise NotImplementedError
+
+    @tf.autograph.experimental.do_not_convert
+    def update_metrics(self, y, mean_pred, std_pred):
+        raise NotImplementedError
+
+    def logdensity(self, x, mu, var):
+        raise NotImplementedError
+
+    def logp(self, F, Y):
+        raise NotImplementedError
+
+    def conditional_mean(self, F):
+        raise NotImplementedError
+
+    def conditional_variance(self, F):
+        raise NotImplementedError
+
+    def predict_mean_and_var(self, Fmu, Fvar):
+        raise NotImplementedError
+
+    def predict_logdensity(self, Fmu, Fvar, Y):
+        raise NotImplementedError
+
+    def variational_expectations(self, Fmu, Fvar, Y):
+        raise NotImplementedError
+
+
+class Gaussian(Likelihood):
+    def __init__(self, log_variance=-5.0, dtype=tf.float64, **kwargs):
+        """"""
+
+        self.log_variance = tf.Variable(
+            initial_value=log_variance, dtype=dtype, name="lik_log_variance"
         )
 
         self.rmse_metric = tf.keras.metrics.RootMeanSquaredError(name="rmse")
 
         self.nll_metric = tf.keras.metrics.Mean(name="nll")
 
-        self.build(0)
+        super().__init__(dtype=dtype, **kwargs)
 
     @property
     def metrics(self):
@@ -45,23 +81,24 @@ class Gaussian(tf.keras.layers.Layer):
         return -0.5 * (np.log(2 * np.pi) + tf.math.log(var) + tf.square(mu - x) / var)
 
     def logp(self, F, Y):
-        return self.logdensity(Y, F, self.variance)
+        return self.logdensity(Y, F, tf.math.exp(self.log_variance))
 
     def conditional_mean(self, F):
         return tf.identity(F)
 
     def conditional_variance(self, F):
-        return tf.fill(tf.shape(F), tf.squeeze(self.variance))
+        return tf.fill(tf.shape(F), tf.squeeze(tf.math.exp(self.log_variance)))
 
     def predict_mean_and_var(self, Fmu, Fvar):
-        return tf.identity(Fmu), Fvar + self.variance
+        return tf.identity(Fmu), Fvar + tf.math.exp(self.log_variance)
 
     def predict_logdensity(self, Fmu, Fvar, Y):
-        return self.logdensity(Y, Fmu, Fvar + self.variance)
+        return self.logdensity(Y, Fmu, Fvar + tf.math.exp(self.log_variance))
 
     def variational_expectations(self, Fmu, Fvar, Y):
+        variance = tf.math.exp(self.log_variance)
         return (
             -0.5 * np.log(2 * np.pi)
-            - 0.5 * tf.math.log(self.variance)
-            - 0.5 * (tf.square(Y - Fmu) + Fvar) / self.variance
+            - 0.5 * tf.math.log(variance)
+            - 0.5 * (tf.square(Y - Fmu) + Fvar) / variance
         )

test.py
@@ -7,17 +7,24 @@ import matplotlib.pyplot as plt
 from src.likelihood import Gaussian
 from src.dvip import DVIP_Base
 from src.layers import VIPLayer
-from src.generative_models import Linear
+from src.generative_models import BayesianLinearNN
+
+import tensorflow as tf
+
+tf.config.run_functions_eagerly(True)
 
 X_train = np.loadtxt("data/SPGP_dist/train_inputs")
 y_train = np.loadtxt("data/SPGP_dist/train_outputs")
+
+y_mean = np.mean(y_train)
+y_std = np.std(y_train)
 
 X_train = X_train[..., np.newaxis]
 
 print(X_train.shape)
 print(y_train.shape)
 
-num_coeffs = 100
+num_coeffs = 10
 
 # Gaussian Likelihood
 ll = Gaussian()
@@ -25,21 +32,21 @@ ll = Gaussian()
 # Layers definition
 rng = default_rng()
 noise_sampler = rng.standard_normal
-net = Linear(noise_sampler, num_coeffs, input_dim=1)
 
 layers = [
-    VIPLayer(net, layer_noise=0.1, num_regression_coeffs=num_coeffs,
-             num_outputs=1, input_dim=1)
+    VIPLayer(noise_sampler, BayesianLinearNN, num_regression_coeffs=num_coeffs,
+             num_outputs=1, input_dim=1),
 ]
 
-dvip = DVIP_Base(ll, layers, X_train.shape[0], num_samples=1)
+dvip = DVIP_Base(ll, layers, X_train.shape[0], num_samples=10, y_mean=y_mean, y_std = y_std)
 
-print(dvip.trainable_variables)
+print(dvip.print_variables())
 
 dvip.compile(optimizer="adam")
 
-dvip.fit(X_train, y_train, epochs=10, batch_size=50)
+dvip.fit(X_train, (y_train - y_mean)/y_std, epochs=200, batch_size=50)
 
+print(dvip.print_variables())
 mean, var = dvip.predict_f(X_train)
 
 plt.scatter(X_train, y_train, color = "blue", label = "Data")