ae0d528e2a67e347c22427ba1852c2bee66e3a25
Ludvins <theludvins@gmail.com>
2021-10-20 11:11:24 +0200

    fix test cases

--- SPGP_data_test.py
+++ SPGP_data_test.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from src.likelihood import Gaussian
 from src.dvip import DVIP_Base
 from src.layers import VIPLayer
-from src.generative_models import NoiseSampler, BayesianLinearNN, BayesianNN2
+from src.generative_models import BayesianLinearNN
 
 import tensorflow as tf
 
@@ -24,33 +24,50 @@ X_train = X_train[..., np.newaxis]
 print(X_train.shape)
 print(y_train.shape)
 
-num_coeffs = 5
+num_coeffs = 20
 
 # Gaussian Likelihood
 ll = Gaussian()
 
 # Layers definition
 rng = default_rng()
-noise_sampler = lambda x: rng.standard_normal(size = x)
+noise_sampler = lambda x: rng.standard_normal(size=x)
 
 layers = [
-    VIPLayer(noise_sampler, BayesianNN2, num_regression_coeffs=num_coeffs,
-             num_outputs=1, input_dim=1),
+    VIPLayer(
+        noise_sampler,
+        BayesianLinearNN,
+        num_regression_coeffs=num_coeffs,
+        num_outputs=1,
+        input_dim=1,
+    ),
 ]
 
-dvip = DVIP_Base(ll, layers, X_train.shape[0],
-                 y_mean=y_mean, y_std=y_std)
+dvip = DVIP_Base(ll, layers, X_train.shape[0], y_mean=y_mean, y_std=y_std)
 
 print(dvip.print_variables())
 
-dvip.compile(optimizer="adam")
+opt = tf.keras.optimizers.Adam(learning_rate=0.1)
 
-dvip.fit(X_train, (y_train - y_mean)/y_std, epochs=200, batch_size=50)
+dvip.compile(optimizer=opt)
+
+callback = tf.keras.callbacks.EarlyStopping(monitor="loss", patience=3)
+
+dvip.fit(X_train, (y_train - y_mean) / y_std, epochs=500, batch_size=32)
 
 print(dvip.print_variables())
-mean, var = dvip.predict_f(X_train)
+mean, var = dvip.predict_y(X_train)
 
-plt.scatter(X_train, y_train, color = "blue", label = "Data")
-plt.scatter(X_train, mean, color = "red", label = "Model fitting")
+sort = np.argsort(X_train[:, 0])
+fig, ax = plt.subplots()
+
+ax.scatter(X_train, y_train, color="blue", label="Data")
+ax.scatter(X_train, mean, color="red", label="Model fitting")
+
+mean = mean.numpy()[sort, 0]
+var = var.numpy()[sort, 0]
+
+ax.fill_between(X_train[sort, 0], mean - 3 * var, mean + 3 * var, color="b", alpha=0.1)
+
 plt.legend()
 plt.show()
