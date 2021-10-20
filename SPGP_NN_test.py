da1131aa1de961d8bbd7d352fc5e63fe4b5f754a
Ludvins <theludvins@gmail.com>
2021-10-20 11:10:56 +0200

    Add test cases

---
+++ SPGP_NN_test.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from numpy.random import default_rng
+
+
+import matplotlib.pyplot as plt
+
+from src.likelihood import Gaussian
+from src.dvip import DVIP_Base
+from src.layers import VIPLayer
+from src.generative_models import BayesianNN, get_bn
+
+import tensorflow as tf
+
+tf.config.run_functions_eagerly(True)
+
+X_train = np.loadtxt("data/SPGP_dist/train_inputs")
+y_train = np.loadtxt("data/SPGP_dist/train_outputs")
+
+y_mean = np.mean(y_train)
+y_std = np.std(y_train)
+
+X_train = X_train[..., np.newaxis]
+
+print(X_train.shape)
+print(y_train.shape)
+
+num_coeffs = 20
+
+# Gaussian Likelihood
+ll = Gaussian()
+
+# Layers definition
+rng = default_rng()
+noise_sampler = lambda x: rng.standard_normal(size=x)
+
+structure = [10, 10]
+activation = tf.keras.activations.tanh
+
+layers = [
+    VIPLayer(
+        noise_sampler,
+        get_bn(structure, activation),
+        num_regression_coeffs=num_coeffs,
+        num_outputs=1,
+        input_dim=1,
+    ),
+]
+
+dvip = DVIP_Base(ll, layers, X_train.shape[0], y_mean=y_mean, y_std=y_std)
+
+print(dvip.print_variables())
+
+opt = tf.keras.optimizers.Adam(learning_rate=0.05)
+# opt = tf.keras.optimizers.SGD(learning_rate=0.01)
+
+dvip.compile(optimizer=opt)
+
+dvip.fit(X_train, (y_train - y_mean) / y_std, epochs=50, batch_size=16)
+
+print(dvip.print_variables())
+mean, var = dvip.predict_y(X_train)
+
+sort = np.argsort(X_train[:, 0])
+fig, ax = plt.subplots()
+
+ax.scatter(X_train, y_train, color="blue", label="Data")
+ax.scatter(X_train, mean, color="red", label="Model fitting")
+
+mean = mean.numpy()[sort, 0]
+std = np.sqrt(var.numpy()[sort, 0])
+
+ax.fill_between(X_train[sort, 0], mean - 3 * std, mean + 3 * std, color="b", alpha=0.1)
+
+plt.legend()
+plt.show()
---
+++ synthetic_test.py
@@ -0,0 +1,88 @@
+import numpy as np
+from numpy.random import default_rng
+
+import matplotlib.pyplot as plt
+
+from src.likelihood import Gaussian
+from src.dvip import DVIP_Base
+from src.layers import VIPLayer
+from src.generative_models import BayesianNN, get_bn
+
+import tensorflow as tf
+
+tf.config.run_functions_eagerly(True)
+
+######################################################
+################### LINEAR DATASET ###################
+######################################################
+
+
+def f(x):
+    return np.cos(5*x)/(np.abs(x) + 1) + default_rng().standard_normal()*0.1
+
+
+X_train = default_rng().standard_normal(300)
+y_train = f(X_train)
+
+y_mean = np.mean(y_train)
+y_std = np.std(y_train)
+
+X_train = X_train[..., np.newaxis]
+
+
+######################################################
+######################## LAYERS ######################
+######################################################
+
+# Regression coefficients
+num_coeffs = 20
+
+# Gaussian Likelihood
+ll = Gaussian()
+
+# Layers definition
+rng = default_rng()
+noise_sampler = lambda x: rng.standard_normal(size=x)
+
+structure = [10, 10]
+activation = tf.keras.activations.tanh
+
+layers = [
+    VIPLayer(
+        noise_sampler,
+        get_bn(structure, activation),
+        num_regression_coeffs=num_coeffs,
+        num_outputs=1,
+        input_dim=1,
+    ),
+]
+
+
+# DVIP
+dvip = DVIP_Base(ll, layers, X_train.shape[0], y_mean=y_mean, y_std=y_std)
+
+print(dvip.print_variables())
+
+opt = tf.keras.optimizers.Adam(learning_rate=0.1)
+
+dvip.compile(opt)
+
+dvip.fit(X_train, (y_train - y_mean) / y_std, epochs=100, batch_size=8)
+
+print(dvip.print_variables())
+mean, var = dvip(X_train)
+
+sort = np.argsort(X_train[:, 0])
+fig, ax = plt.subplots()
+
+
+ax.scatter(X_train, y_train, color="blue", label="Data")
+ax.scatter(X_train, mean, color="red", label="Model fitting")
+
+mean = mean.numpy()[sort, 0]
+var = var.numpy()[sort, 0]
+
+ax.fill_between(X_train[sort, 0], mean - 3 * var, mean + 3 * var, color="b", alpha=0.1)
+
+plt.legend()
+plt.show()
